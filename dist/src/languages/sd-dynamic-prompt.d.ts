f94e56deaa4bdf56467aa12800c0165385baf0ed
Taremin <taremin.vr@gmail.com>
2023-07-22 00:09:51 +0900

    v0.0.8

--- dist/src/languages/sd-dynamic-prompt.d.ts
+++
@@ -1,4 +0,0 @@
-import { languages } from 'monaco-editor/esm/vs/editor/editor.api';
-declare const conf: languages.LanguageConfiguration;
-declare const language: languages.IMonarchLanguage;
-export { conf, language, };
